71ee41554d56f544a87250409e28aa9cde3970b0
Léa Brugière <brugierelea@gmail.com>
2025-09-18 17:44:42 +0200

    FEAT:page home

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import '../Card/Card.scss';
+
+export default function Card({cover, title}) {
+    return (
+        <article className='card'>
+            <img className='card_image' src={cover} alt='location' />
+            <p className='card_title'>{title}</p>
+        </article>
+    );
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,23 @@
 import Banner from '../../components/Banner/Banner';
 import imgHomeBanner from '../../assets/images/banner-home.png';
+import lodgings from '../../assets/data/lodgings.json';
+import Card from '../../components/Card/Card';
+import { Link } from 'react-router-dom';
+import '../Home/Home.scss';
 
 export default function Home() {
     return (
+        <>
         <Banner  text='Chez vous, partout et ailleurs' image={imgHomeBanner} />
+        <section className='cards-container'>
+            {lodgings.map((appart)=>(
+                <div className='card_log' key= {appart.id}>
+                    <Link className='link_card_log' to={`/Lodging/${appart.id}`}>
+                    <Card cover={appart.cover} title={appart.title} />
+                    </Link>
+                </div>
+            ))}
+        </section>
+        </>
     )
 }
