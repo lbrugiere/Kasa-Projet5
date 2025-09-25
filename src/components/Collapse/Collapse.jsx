c63d276f5b9af8d0a8e24d524d9831fc7d21bef3
Léa Brugière <brugierelea@gmail.com>
2025-09-25 18:36:49 +0200

    FEAT: page à propos

---
+++ src/components/Collapse/Collapse.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "../Collapse/Collapse.scss";
+
+export default function Collapse({ title , content }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <div className="collapse">
+            <div className="collapse_title">
+                <h2>{title}</h2>
+                <i 
+                onClick={ () => setIsOpen(!isOpen)}
+                className={`fa-solid fa-chevron-up ${isOpen ? "rotate" : ""}`}></i>
+            </div>
+            <div className={isOpen ? "collapse_content open" : "collapse_content"}>
+            {isOpen && <p>{content}</p>}
+            </div>
+        </div>
+    )
+}
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,3 +1,18 @@
+import Collapse from "../../components/Collapse/Collapse";
+import imgAboutBanner from "../../assets/images/banner-about.png";
+import Banner from "../../components/Banner/Banner.jsx";
+import about from "../../assets/data/about.json";
+import "../About/About.scss";
+
 export default function About() {
-    return <h1>About</h1>;
+    return (
+        <>
+        <Banner image={imgAboutBanner}/>
+        <section className="container-about">
+            {about.map(({ title, content}) => (
+                < Collapse  key={title} title={title} content={content} />
+            ))}
+        </section>
+        </>
+    )
 }
