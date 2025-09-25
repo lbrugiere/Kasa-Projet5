60c0b11b3158494ec78be7534b19b48498bb5858
Léa Brugière <brugierelea@gmail.com>
2025-09-25 19:22:37 +0200

    FEAT: page erreur

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+import "../Error/Error.scss";
+
 export default function Error() {
-    return <h1>Erreur</h1>;
+    return (
+        <div className="error-container">
+            <h2>404</h2>
+            <p>Oups! La page que vous demandez n'existe pas!</p>
+            <Link to="/">Retourner sur la page d'accueil</Link>
+        </div>
+    )
 }
